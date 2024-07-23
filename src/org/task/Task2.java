109246f580eb2ec183e6984938ec362318bd7a03
Varshini <sabari391111@gmail.com>
2024-07-23 12:00:21 +0530

    test

--- src/org/task/Task2.java
+++ src/org/task/Task2.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task2 {
+	
 public static void main(String[] args) throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
